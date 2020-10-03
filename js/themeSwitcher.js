3a3bee75d2889593f4d854ffdba7902321e64797
VitorGouveia <61664367+VitorGouveia@users.noreply.github.com>
2020-10-03 17:48:44 -0300

    organização de arquivos e 3 temas
    novo tema adicionado, theme-contrast, ainda precisa de mudança nas cores

---
+++ js/themeSwitcher.js
@@ -0,0 +1,30 @@
+    // function to set a given theme/color-scheme
+  function setTheme(themeName) {
+    localStorage.setItem("theme", themeName);
+    document.documentElement.className = themeName;
+  }
+  
+  // function to toggle between light and dark theme
+  function toggleTheme() {
+    if (localStorage.getItem("theme") === "theme-dark") {
+      setTheme("theme-light");
+    } else if(localStorage.getItem("theme") === "theme-contrast") {
+      setTheme("theme-dark");
+    } else {
+      setTheme("theme-contrast")
+    }
+  }
+  
+  // Immediately invoked function to set the theme on initial load
+  (function () {
+    if (localStorage.getItem("theme") === "theme-dark") {
+      setTheme("theme-dark");
+      document.getElementById("slider").checked = false;
+    } else if (localStorage.getItem("theme") === "theme-light"){
+      setTheme("theme-light");
+      document.getElementById("slider").checked = true;
+    } else {
+      setTheme("theme-contrast");
+      document.getElementById("slider").checked = true;
+    }
+  })(); 
