aa422ff07ce7fb35a5e5c8cc393a2fc2579b4781
VitorGouveia <61664367+VitorGouveia@users.noreply.github.com>
2020-11-04 22:58:33 -0300

    Service worker funcionando

--- serviceWorker.js
+++ serviceWorker.js
@@ -5,13 +5,12 @@ self.addEventListener("install", e => {
                 [
                     "./",
                     "./css/index.css",
-                    "./css/propsito.css",
+                    "./paginas/proposito.html",
+                    "./css/proposito.css",
                     "./recursos/images/favicon/etecFavicon-128px.png",
                     "./recursos/images/favicon/etecFavicon-144px.png",
                     "./recursos/images/favicon/etecFavicon-152px.png",
                     "./recursos/images/favicon/etecFavicon-192px.png",
-                    "./recursos/images/favicon/etecFavicon-384px.png",
-                    "./recursos/images/favicon/etecFavicon-512px.png",
                     "./recursos/images/background-images/etecjrm.jpg",
                     "./recursos/images/background-images/etecjrm2.jpg",
                     "./recursos/svg/etec.svg"
